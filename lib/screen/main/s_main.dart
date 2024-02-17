2dad896a15b237cde86390498a81f479b335da80
RichardKim <hiddeneventor@gmail.com>
2024-02-17 23:33:01 +0900

    [토스앱] 하단 네비게이션 화면 작업

--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -20,7 +20,7 @@ class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMi
   final tabs = [
     TabItem.home,
     TabItem.benefit,
-    TabItem.ttosspay,
+    TabItem.ttospay,
     TabItem.stock,
     TabItem.all,
   ];
