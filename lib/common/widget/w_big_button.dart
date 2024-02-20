223df1243b98a450d7059404795e0c845429597f
RichardKim <hiddeneventor@gmail.com>
2024-02-20 23:35:02 +0900

    [토스앱] big button 위젯 추가

---
+++ lib/common/widget/w_big_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BigButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  const BigButton(this.text, {super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,6 +1,5 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/common/widget/round_button_theme.dart';
-import 'package:fast_app_base/common/widget/w_round_button.dart';
+import 'package:fast_app_base/common/widget/w_big_button.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -17,9 +16,18 @@ class HomeFragment extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
-      child: const Stack(
+      child: Stack(
         children: [
-          SingleChildScrollView(child: Column()),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                BigButton(
+                  '토스뱅크',
+                  onTap: () => context.showSnackbar('토스뱅크 버튼 클릭'),
+                )
+              ],
+            ),
+          ),
           TtossAppBar(),
         ],
       ),
